57ac1340e61c6a5128b366d8932ee19aef5e5386
Philip Fahim <philip.louis49@gmail.com>
2018-11-12 15:47:02 -0800

    fix: throw firebase api into secret file

---
+++ secret.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAd839uMpDUlmyPn0ddkVzO5nX5D_OuIRE",
+    authDomain: "authentication-eab28.firebaseapp.com",
+    databaseURL: "https://authentication-eab28.firebaseio.com",
+    projectId: "authentication-eab28",
+    storageBucket: "authentication-eab28.appspot.com",
+    messagingSenderId: "734338637057"
+}
